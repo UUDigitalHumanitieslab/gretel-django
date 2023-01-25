6ed8b2f64468f66f73b8c7d1f24e76c7619ffd35
ben <b.bonfil@uu.nl>
2023-01-25 13:49:09 +0100

    temporary hack to reduce RAM usage by avoiding sastadev.celexlexicon

--- backend/gretel/__init__.py
+++ backend/gretel/__init__.py
@@ -1,3 +1,9 @@
 from .celery import app as celery_app
 
 __all__ = ('celery_app',)
+
+# FIXME this is a temporary hack to prevent the loading of the CELEX lexicon by sastadev
+# and thus save quite some RAM usage.
+# To be remove once https://github.com/UUDigitalHumanitieslab/sastadev/pull/9 is merged
+import sys
+sys.modules['celexlexicon'] = type(sys)('celexlexicon')
